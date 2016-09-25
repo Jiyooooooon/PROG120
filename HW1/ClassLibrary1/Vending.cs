09b81b241b662ff61007c2e1ceccc7f51fd276ad
Jiyoooooon <yooon87@gmail.com>
2016-09-25 16:19:59 -0700

    First commit

---
+++ HW1/ClassLibrary1/Vending.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Vending
+    {
+        private int _MachineBalance;
+        private int _Bottles;
+        
+        public Vending()
+        {
+            _MachineBalance = 0;
+            _Bottles = 5; 
+        }
+
+        public int Bottle
+        {
+            get
+            {
+                return _Bottles;
+            }     
+        }
+        public int Balance
+        {
+            get
+            {
+                return _MachineBalance;
+            }
+        }
+        public int BuyCoke()
+        {
+            if (_Bottles > 0 && _MachineBalance > 0) //_Bottles != 0 is also correct!
+            {
+                _Bottles = _Bottles - 1;
+                _MachineBalance = _MachineBalance - 1;
+                return 0;  
+            }
+            else if (_Bottles == 0)
+                return 1;
+            else if (_MachineBalance == 0)
+                return 2;
+            else
+                return 3;
+        }
+        public void AcceptCash()
+        {
+            _MachineBalance = _MachineBalance + 1;
+        }
+        public int GiveRefund()
+        {
+            int temp = _MachineBalance;
+            _MachineBalance = 0;
+                return temp;
+        }
+       
+    }
+}
---
+++ HW1/VendingMaching/Program.cs
@@ -0,0 +1,80 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMaching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string customerCommand;
+            
+            Vending Coke = new Vending();
+
+            Console.WriteLine("Welcome!");
+            Console.WriteLine();
+            Console.WriteLine("Please type P to insert a dollar,");
+            Console.WriteLine("or B to buy a Coke");
+            Console.WriteLine("or R to get all your money back");
+            Console.WriteLine("or Q to quit this program");
+            Console.WriteLine();
+            customerCommand = Console.ReadLine();
+            customerCommand = customerCommand.ToUpper();
+            while (customerCommand != "Q")
+            {
+                switch (customerCommand)
+                {
+                    case "P":
+                        Coke.AcceptCash();
+                        Console.WriteLine("Thank you, you now have ${0}", Coke.Balance);
+                        break;
+                    case "B":
+                        switch (Coke.BuyCoke())
+                        {
+                            case 0:
+                                Console.WriteLine("Thank you for your purchase, you have ${0} left.", Coke.Balance);
+                                break;
+                            case 1:
+                                Console.WriteLine("Sorry, the machine is empty. Enter an R to get your money back.");
+                                break;
+                            case 2:
+                                Console.WriteLine("Sorry, you need to insert a dollar.");
+                                break;
+                            case 3:
+                                Console.WriteLine("Sorry, you need to insert a dollar and the machince is empty.");
+                                break;
+                            default:
+                                Console.WriteLine("Invaild selection");
+                                break;
+                        }
+                        break;
+                    case "R":
+                        if (Coke.Balance != 0)
+                        {
+                            Console.WriteLine("Here is your ${0}", Coke.GiveRefund());
+                        }
+                        else
+                        {
+                            Console.WriteLine("sorry, you don't have that much money");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Invaild selection");
+                        break;
+                }
+                Console.WriteLine("Please type P to insert a dollar,");
+                Console.WriteLine("or B to buy a Coke");
+                Console.WriteLine("or R to get all your money back");
+                Console.WriteLine("or Q to quit this program");
+                customerCommand = Console.ReadLine();
+                customerCommand = customerCommand.ToUpper();
+            }
+            Console.WriteLine("Have a good day, See you later!");
+            Console.ReadLine();
+        }
+    }
+}
